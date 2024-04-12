5cda7c8f9109525992e3f660bf367945df4b2a97
MIKCYY <mickysamson31@gmail.com>
2024-04-12 18:11:50 +0300

    chat app all front and backend

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,17 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <PrettyChatWindow
+        projectId='
+09e08f5f-0364-4747-9017-849d33a971e6'
+        username={props.user.username} // adam
+        secret={props.user.secret} // pass1234
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
